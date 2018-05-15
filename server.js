3c41ab4637a3df672d1d00c49aa3a292509460d4
Chris Richards <crichards999@gmail.com>
2018-05-15 19:00:48 +0000

    Added a bunch of stuff

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var server = require('http').Server(app);
+
+app.get('/', function(req, resp){
+    resp.send('<h1>Hello World!</h1>');
+});
+
+server.listen(8080, function(){
+    console.log('Listening on *:3000...');
+    
+});
