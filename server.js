8f4e4f987fac1bd2946caa11fdb564402f353b19
Chris Richards <crichards999@gmail.com>
2018-05-16 16:51:29 +0000

    Added Socket.IO Chat Functionality

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var app = require('express')();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 app.get('/', function(req, resp){
     resp.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected.');
+    
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    
+    socket.on('chat message', function(msg){
+        console.log('message: ', msg);
+        io.emit('chat message', msg);
+    });
 });
 
 server.listen(8080, function(){
