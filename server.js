1bc2c9f6d0c80dfd70841bd5c1ca5d616938b4d5
Chris Richards <crichards999@gmail.com>
2018-05-16 14:03:40 +0000

    Changed Express to Serve HTML File instead of sending text.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var server = require('http').Server(app);
 
 app.get('/', function(req, resp){
-    resp.send('<h1>Hello World!</h1>');
+    resp.sendFile(__dirname + '/index.html');
 });
 
 server.listen(8080, function(){
